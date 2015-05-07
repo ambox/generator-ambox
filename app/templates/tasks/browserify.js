82c23593ff807180d769af8cb139a1f80537c138
Adrian Miranda <adrian.miranda@isobar.com.br>
2015-05-07 20:44:29 -0300

    browserify-shim

--- app/templates/tasks/browserify.js
+++ app/templates/tasks/browserify.js
@@ -4,14 +4,12 @@ module.exports = function (grunt) {
 	// Grunt task for node-browserify.
 	// @see https://github.com/jmreidy/grunt-browserify
 	// @see https://github.com/componentjs/component
-	// @see https://github.com/eugeneware/decomponentify
-	// @see https://www.npmjs.com/package/aliasify/
-	// @see https://github.com/eugeneware/debowerify
+	// @see https://github.com/thlorenz/browserify-shim
 	// @see https://github.com/jaredhanson/deamdify
 	// @see https://github.com/eugeneware/deglobalify
 	return {
 		options: {
-			transform: ['decomponentify', 'aliasify', 'debowerify', 'deamdify', 'deglobalify'],
+			transform: ['browserify-shim', 'deamdify', 'deglobalify'],
 			browserifyOptions: {
 				extensions: ['.js', '.jsx', '.coffee'],
 				debug: true,
