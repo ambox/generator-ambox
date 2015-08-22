e036ef455b6e2a475aa79c262ef514b8df2762de
Adrian Miranda <adrian.miranda@isobar.com.br>
2015-08-22 17:17:54 -0300

    requirejs task modules

---
+++ app/templates/tasks/requirejs.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt, options) {
+	'use strict';
+
+	var config = {};
+
+	// Run each command for each modules
+	options.scaffold.modules.forEach(function (module) {
+		// Optimize RequireJS projects using r.js
+		// @see https://github.com/gruntjs/grunt-contrib-requirejs
+		config[module] = {
+			options: {
+				baseUrl: '<%= scaffold.src %>/scripts',
+				include: ['requirejs', 'config-' + module],
+				mainConfigFile: '<%= scaffold.src %>/scripts/config-' + module + '.js',
+				out: '<%= scaffold.tmp %>/scripts/body-' + module + '.min.js',
+				optimize: 'none', // none|uglify2
+				optimizeCss: 'none',
+				uglify2: { mangle: false },
+				fileExclusionRegExp: /^(.git|node_modules)$/,
+				preserveLicenseComments: false,
+				findNestedDependencies: true,
+				generateSourceMaps: false,
+				removeCombined: true,
+				useStrict: true,
+				wrap: {
+					start: '(function(window, document, undefined){',
+					end: '})(this, document);'
+				}
+			}
+		};
+	});
+	
+	// @see tasks/scaffold.js
+	return config;
+};
