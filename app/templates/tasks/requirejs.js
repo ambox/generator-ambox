f3108a4846dc1e1dd521d09b76f1156ccbe92e52
Adrian Miranda <adrian.miranda@isobar.com.br>
2015-08-23 22:38:41 -0300

    RequireJS uglify2

--- app/templates/tasks/requirejs.js
+++ app/templates/tasks/requirejs.js
@@ -6,14 +6,14 @@ module.exports = function (grunt, options) {
 	// Run each command for each modules
 	options.scaffold.modules.forEach(function (module) {
 		// Optimize RequireJS projects using r.js
-		// @see https://github.com/gruntjs/grunt-contrib-requirejs
+		// @see https://github.com/jrburke/r.js/blob/master/build/example.build.js
 		config[module] = {
 			options: {
 				baseUrl: '<%= scaffold.source %>/scripts',
 				include: ['requirejs', 'config-'+ module],
 				mainConfigFile: '<%= scaffold.source %>/scripts/config-'+ module +'.js',
 				out: '<%= scaffold.static %>/scripts/body-'+ module +'.min.js',
-				optimize: 'none', // none|uglify2
+				optimize: 'uglify2', // none|uglify2
 				optimizeCss: 'none',
 				uglify2: { mangle: false },
 				fileExclusionRegExp: /^(.git|node_modules)$/,
