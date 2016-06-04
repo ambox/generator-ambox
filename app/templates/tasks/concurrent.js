f526c7b47f505a29ef4a86c86deffa45b72f0135
Adrian C. Miranda <adriancmiranda@gmail.com>
2016-06-04 16:41:13 -0300

    cleaning

--- app/templates/tasks/concurrent.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (grunt) {
-	'use strict';
-
-	// Run grunt tasks concurrently.
-	// @see https://github.com/sindresorhus/grunt-concurrent
-	return {
-		images: ['newer:imagemin', 'newer:svgmin']
-	};
-};
